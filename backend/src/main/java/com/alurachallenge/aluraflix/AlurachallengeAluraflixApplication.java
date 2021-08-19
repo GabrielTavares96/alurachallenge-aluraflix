a57a7bbb9ef75ad698eb5eb39dee5ac8dbc3cfa5
GabrielTavares96 <gabriel_sbo1996@hotmail.com>
2021-08-19 10:02:27 -0300

    Project created

---
+++ backend/src/main/java/com/alurachallenge/aluraflix/AlurachallengeAluraflixApplication.java
@@ -0,0 +1,13 @@
+package com.alurachallenge.aluraflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlurachallengeAluraflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlurachallengeAluraflixApplication.class, args);
+	}
+
+}
